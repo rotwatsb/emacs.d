de5dd821fd0643d9b16cc36bd7cc977039a3eec9
Stephen Bradley <sbradley@opengov.com>
2022-07-12 10:13:18 -0400

    configure org roam

--- init.el
+++ init.el
@@ -5,6 +5,21 @@
 
 (package-initialize)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+
 ;; This loads the actual configuration in literate org-mode elisp
 (defun load-config()
   (interactive)
